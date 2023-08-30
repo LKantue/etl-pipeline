e288ce5925452110ff221789a2f53d6e2d5bf31b
lkantue <leratokantue.com>
2023-08-30 15:45:38 +0200

    All ETL steps

---
+++ src/transform.py
@@ -0,0 +1,10 @@
+def remove_duplicates(df):
+    if df.duplicated().sum() > 0:
+        print(f"number of duplicates = {df.duplicated().sum()}")
+        df_cleaned = df.drop_duplicates(keep='first')
+
+    else:
+        df_cleaned = df
+        print("no duplicates in dataframe")
+
+    return df_cleaned
